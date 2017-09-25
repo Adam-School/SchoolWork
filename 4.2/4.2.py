c1c3c9c2fe406c86c645236fa13f3fba255cb90e
Adam-School <32051510+Adam-School@users.noreply.github.com>
2017-09-24 23:15:59 -0500

    Add files via upload

--- 4.2/4.2.py
+++ 4.2/4.2.py
@@ -1,10 +1,12 @@
 money = float(.01)
+tot = 0
 y = 1
 days = int(input('How many days will you work for pennies a day? '))
 print ('Days Worked | Amount Earned That Day')
 print('------------------------------------')
 for x in range (days):
     print(y,'\t',' | ' ,'$',(format(money, ',.2f')))
+    tot += money
     money *= 2
     y += 1
 print('Total earned over ',days,' days is ','$',(format(money, ',.2f')))
