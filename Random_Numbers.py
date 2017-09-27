5f881dfa4bb4dc52c5a06bee3d8827b5499fc181
Adam-School <32051510+Adam-School@users.noreply.github.com>
2017-09-27 18:58:36 -0500

    Add files via upload

---
+++ Random_Numbers.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+
+def main():
+    # get a random number
+    number = random.randint(1, 10)
+    # display the random number
+    time.sleep(1)
+    print('The number is ', number)
+
+
+main()
