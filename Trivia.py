9d1beb18fcceb849eb5ec2b9bda9eedb9d3cdbd5
Adam-School <32051510+Adam-School@users.noreply.github.com>
2017-11-13 22:59:55 -0600

    Update Trivia.py

--- Trivia.py
+++ Trivia.py
@@ -1,4 +1,5 @@
 class questions():
+    #init statement to make the basic variables
     def __init__(self):
         self.questions = []
         self.answers = []
@@ -23,27 +24,28 @@ def main():
 
     quests = []
     answers = []
+    #make a list of answers
     answers = ['Emoji','Face Sticker','Person Face','Faces','Liquid Cyinide Display','Light Crystal Display','Light Circle Display',
                'Liquid Crystal Display','Real on Memory', 'Read on Memory','Read only Memory','Real Occulas Memory','Computer Disk Reads only Memory',
                'Computer Data Reads on Memory','Computer Data Right on Memory','Compact Disk Read Only Memory','Iron','Steel','Alumminum',
                'Copper','Hydrogen','Oxygen','Carbon','Iron','Atlantic Ocean','Pacific Ocean','Indian Ocean','Artic Ocean','June','August',
                'December','January','Sargasso Sea','Black Sea','Boltic Sea','Red Sea','Iron','Oxygen','Nitrogen','Hydrogen']
-
+    #making questions into a list
     quests.append('What do you call the small image icons used to express emotions or ideas in digital communication?')
     quests.append('When referring to a computer monitor, what does the acronym LCD stand for?')
     quests.append('When talking about computer memory, what does the acronym ROM stand for?')
     quests.append('What do the letters in the acronym CD-ROM stand for?')
     quests.append('Which is the most abundant metal in the earths crust?')
-
+    
     quests.append('What is the second most abundant element in the earths atmosphere?')
     quests.append('What is the worlds largest ocean?')
     quests.append('In what month is the Earth closest to the sun?')
     quests.append('What is the only sea on Earth with no coastline?')
     quests.append('What is the most abundant element in the earths atmosphere?')
-
+    #sending the questions to b stored in the object in class
     for r in range(0,10):
         quest.setQuestion(quests[r])
-
+    #sending the answers to be stored in the object in class
     for x in range(0,40):
         quest.setAnswer(answers[x])
     print('-----------------------------------------------------------------------------------------------------------')
